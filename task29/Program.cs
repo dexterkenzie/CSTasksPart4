2f4c410f5b3395366bace854ec4007bbfadd2ae2
«dashkovsky» <>
2022-09-11 10:22:49 +0300

    task 29 done

--- task29/Program.cs
+++ task29/Program.cs
@@ -5,3 +5,14 @@
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите числа через пробел: ");
+string numbers = Console.ReadLine();
+string[] numbersList = numbers.Split(' ');
+
+Console.Write("Ваш массив: ");
+
+foreach (var item in numbersList) {
+    Console.Write($"'{item}' ");
+}
+

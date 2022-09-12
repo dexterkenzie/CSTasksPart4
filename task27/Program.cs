c677da4067dda93e60dae218a5e946fd270465e0
«dashkovsky» <>
2022-09-12 20:59:58 +0300

    task 27 done

--- task27/Program.cs
+++ task27/Program.cs
@@ -7,3 +7,16 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Вы ввели число: {number}"); // проверка ввода данных
+
+int result = 0;
+while (number > 0)
+{
+    int dig = number % 10;
+    result = result + dig;
+    number = number / 10;
+}
+
+Console.WriteLine($"Сумма цифр равна: {result}");
